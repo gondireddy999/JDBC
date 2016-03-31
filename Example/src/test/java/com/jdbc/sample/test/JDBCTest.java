adc58063165937be0c2c68efa3102d7c2de1ef3f
Malli Reddy <gondireddy999@gmail.com>
2016-03-31 10:12:49 +0530

    Converting into Maven Project

---
+++ Example/src/test/java/com/jdbc/sample/test/JDBCTest.java
@@ -0,0 +1,32 @@
+package com.jdbc.sample.test;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.jdbc.sample.EmployeeDAO;
+
+public class JDBCTest {
+
+	/**
+	 * @param args
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
+	 */
+	@Test
+	public void main() {
+		// TODO Auto-generated method stub
+
+		EmployeeDAO jdbcTest=new EmployeeDAO();
+		System.out.println("n:::Get All emplyess:::\n");
+		jdbcTest.getEmployees();
+		System.out.println("\n:::Get All emplyees by id:::\n");
+		jdbcTest.getEmployeeById(1);
+		System.out.println("\n:::Saving Employee:::\n");
+		int insertRes=jdbcTest.saveEmployee(125, "empName");
+		
+		System.out.println("RESSSSS::::"+insertRes);
+		
+	}
+
+}
---
+++ Example/src/test/java/com/jdbc/sample/test/MysqlCon.java
@@ -0,0 +1,39 @@
+package com.jdbc.sample.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+class MysqlConTest {
+
+	@Test
+	public  void mysqlConTest() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/test", "root", "root");
+
+			// here sonoo is database name, root is username and password
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("select * from employee");
+
+			while (rs.next())
+				// System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+				System.out.println(rs.getInt(1) + "  " + rs.getDate(2) + "  "
+						+ rs.getString(3) + "  " + rs.getDouble(4) + " "
+						+ rs.getString(5));
+
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+}
